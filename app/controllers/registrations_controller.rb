5ddf5e167edf04a48af05b1faedc7a8ad7f917a8
Roman <romtega88@gmail.com>
2016-01-14 10:48:52 -0600

    Added extra user attributes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ private
 	end
 
 	def account_update_params
-		params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
+		params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password, :age, :gender, :bio)
 	end
 
 end
